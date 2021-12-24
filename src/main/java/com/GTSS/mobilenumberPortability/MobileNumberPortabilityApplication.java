a86aa6cf5842ee46c5cb1285ed7cce910c8a2a08
Mostafa Hares <mostafahares698@gmail.com>
2021-12-24 21:37:15 +0200

    Intial Commit

---
+++ src/main/java/com/GTSS/mobilenumberPortability/MobileNumberPortabilityApplication.java
@@ -0,0 +1,13 @@
+package com.GTSS.mobilenumberPortability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileNumberPortabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileNumberPortabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GTSS/mobilenumberPortability/MobileNumberPortabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GTSS.mobilenumberPortability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileNumberPortabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
